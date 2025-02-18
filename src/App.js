2c3f66cba1b6f9c20313c2e0fbf1eccdd05a2764
QAguy321 <muhammad.sqa8@gmail.com>
2025-02-18 10:19:37 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+        Test page for Boss
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
